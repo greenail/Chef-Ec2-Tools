72d6ed63e07ff076d1d9415e4c830cdfd5cc973a
jschoch <greenail@yahoo.com>
2010-09-14 16:40:03 +0000

    updated push

--- push.sh
+++ push.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/ruby
+unless File.exists? ARGV[0]
+	system("git add #{ARGV[0]}/*")
+end
 system("git commit -a -m 'more'")
 system("knife cookbook upload #{ARGV[0]} -o /local/chef-repo/cookbooks")
 
